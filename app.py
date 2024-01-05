6cabac7bf6a1d7e02f5ae3bc196509a9d119caba
Kushwanth Chowday Kandala <kushwanthkandala@gmail.com>
2024-01-05 02:21:42 -0800

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 import os
+from streamlit_chat import message
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
@@ -42,10 +43,10 @@ for i in st.session_state["chat_history"]:
         st.write(i["content"])
 
 
-import numpy as np
-from PIL import Image
+# import numpy as np
+# from PIL import Image
 
-img_file_buffer = st.file_uploader('Upload a PNG image', type='png')
-if img_file_buffer is not None:
-    image = Image.open(img_file_buffer)
-    img_array = np.array(image)
+# img_file_buffer = st.file_uploader('Upload a PNG image', on_change= type=['png','jpg'])
+# if img_file_buffer is not None:
+#     image = Image.open(img_file_buffer)
+#     img_array = np.array(image)
