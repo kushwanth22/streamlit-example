5361b5cfecd5d0fc1a3a27b4a92a03670a4dea22
Kushwanth Chowday Kandala <kushwanthkandala@gmail.com>
2024-01-05 01:42:43 -0800

    update chat app.py

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 import streamlit as st
-# from IPython.display import display
-# from IPython.display import Markdown
+import google.generativeai as genai
 import os
 
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
-
-
-# def to_markdown(text):
-#   text = text.replace('•', '  *')
-#   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True)
-
-import google.generativeai as genai
 
 genai.configure(api_key = gemini_api_key)
 
@@ -19,37 +11,44 @@ model = genai.GenerativeModel('gemini-pro')
 
 
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
-else:
-    prompt = "who are you?"
-response = model.generate_content(prompt)
-message = st.chat_message("ai")
-message.write(response.text)
-
-# # Define a function that will be called when the user clicks the button
-# def greet_user():
-#     # Get the user's name from the text input
-#     name = st.text_input("Enter your name:")
-
-#     # Greet the user
-#     st.write(f"Hello, {name}!")
-
-# # Add a button to the app
-# st.button("Greet me!", greet_user)
+# prompt = st.chat_input("Say something")
+# if prompt:
+#     st.write(f"User has sent the following prompt: {prompt}")
+# else:
+#     prompt = "who are you?"
+# response = model.generate_content(prompt)
+# message = st.chat_message("ai")
+# message.write(response.text)
 
 
-
-# st.title("Hello! Streamlit App")
-
-# x = st.slider('Select a value')
-# st.write(x, 'squared is', x * x)
+import string
+import random
 
 
-# st.title('AI Fitness Trainer: Squats Analysis')
+def randon_string() -> str:
+    return "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
 
-# recorded_file = 'output_sample.mp4'
-# sample_vid = st.empty()
-# sample_vid.video(recorded_file)
+def chat_actions():
+    st.session_state["chat_history"].append(
+        {"role": "user", "content": st.session_state["chat_input"]},
+    )
+
+    response = model.generate_content(st.session_state["chat_input"])
+    st.session_state["chat_history"].append(
+        {
+            "role": "assistant",
+            "content": response,
+        },  # This can be replaced with your chat response logic
+    )
+
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+
+st.chat_input("Enter your message", on_submit=chat_actions, key="chat_input")
+
+for i in st.session_state["chat_history"]:
+    with st.chat_message(name=i["role"]):
+        st.write(i["content"])
