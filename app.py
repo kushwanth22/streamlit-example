65ac042b21d5a61767d5193beccef98b1dc73b70
Kushwanth Chowday Kandala <kushwanthkandala@gmail.com>
2024-01-12 21:14:44 +0000

    update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ for i in st.session_state["chat_history"]:
 import numpy as np
 from PIL import Image
 
-img_file_buffer = st.file_uploader('Upload a PNG image', on_change=uploader_callback type=['png','jpg'])
+img_file_buffer = st.file_uploader('Upload a PNG image', on_change=uploader_callback, type=['png','jpg'])
 if img_file_buffer is not None:
     image = Image.open(img_file_buffer)
     img_array = np.array(image)
